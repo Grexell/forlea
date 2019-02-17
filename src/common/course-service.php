5c0ffc3e701c42e445c9a5bb4a9aec13aec5a76d
demes <demesh.dima.dd@gmail.com>
2019-02-17 23:02:45 +0300

    init backend

---
+++ src/common/course-service.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: demes
+ * Date: 17.02.2019
+ * Time: 11:59
+ */
---
+++ src/common/user-service.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+function is_authorised()
+{
+    return empty($_SESSION['username']) || empty($_SESSION['password']);
+}
+
+// todo ont't need it with ajax
+function is_authorized_or_redirect()
+{
+    $authorized = is_authorised();
+    if (!$authorized) {
+        header('Location: /login.php');
+    }
+    return $authorized;
+}
+
+function authorize($username, $password)
+{
+// todo return state of authorization
+//    todo: retrieve and check info from db
+
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+}
+
+function logout()
+{
+    unset($_SESSION['username']);
+    unset($_SESSION['password']);
+}
+
+function register()
+{
+
+}
+
+?>
